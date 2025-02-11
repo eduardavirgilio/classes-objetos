b95c295e4fbd3c06e485f3db15a7c6f269152edc
Eduarda Virgilio <eduarda.virgilio2630@gmail.com>
2025-02-11 14:47:29 -0300

    first commit

---
+++ 01funcoes.js
@@ -0,0 +1,72 @@
+// não precisa de parametros
+function somar(){
+    let v1 = 0;
+    let v2 = 0;
+    const resultado = v1 + v2;
+    console.log(resultado);
+}
+
+somar(); //invocando a função
+
+//função com parametros (nao tem o return)
+function somar(v1, v2){
+    const resultado = v1 + v2;
+    console.log(resultado);
+}
+
+console.log(somar(1, 2));
+
+//retornando o valor do calculo (com return)
+function somar(v1, v2){
+    const resultado = v1 + v2;
+    return resultado;
+}
+
+console.log(somar(1, 2));
+
+//retornando valor informado no parametro versao simplificada
+function somar(v1, v2){
+    return v1 + v2;
+}
+
+// criando uma função com o html de soma
+
+function somar(){
+    const v1 = document.getElementById('valor-01').value;
+    const v2 = document.getElementById('valor-02').value;
+    somar(v1, v2);
+    document.getElementById.('resultado').textContent = `${somar()}` //o textcontent coloca dentro de uma tag 
+}
+
+//resolução porcentagem - função normal
+//função que retorna a % de um valor
+function porcentagem(valor, taxa){
+    return valor * (taxa / 100);
+}
+
+//resolução porcentagem - função de seta
+porcentagem = (valor, taxa) =>{
+    return valor * (taxa / 100);
+}
+
+//resolução compra prazo - função normal
+//função que retorna o valor de uma compra a prazo
+function compraPrazo(valor, taxa){
+    return valor + (valor * taxa / 100);
+}
+
+//resolução compra prazo - função de seta
+compraPrazo = (valor, taxa) =>{
+    return valor + (valor * taxa / 100);
+}
+
+//resolução compra vista - função normal
+//função que retorna o valor de uma compra a vista
+function compraVista(valor, taxa){
+    return valor - (valor * taxa / 100);
+}
+
+//resolução compra vista - função de seta
+compraVista = (valor, taxa) => {
+    return valor - (valor * taxa / 100);
+}
